9e5fcaf662f1640f2975cd6ecae2a791340f2a56
camden.warburton <camden.warburton@ucalgary.ca>
2024-03-22 16:42:40 -0600

    Initial commit, added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About'
+import Layout from './pages/Layout';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        
+        </Route> 
+        {/* This is the end of Layout */}
+      
+    </Routes>
+  </BrowserRouter>
+ 
   );
 }
 
