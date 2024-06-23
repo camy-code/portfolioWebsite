ee122906e98eba48f4ac2f53df580f21fb7ae558
camden.warburton <camden.warburton@ucalgary.ca>
2024-06-22 22:14:34 -0600

    added back end shit

---
+++ src/services/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQEWMZ1yGvJym2CZAPybtikEUGRQIBZA8",
+  authDomain: "camport-b643b.firebaseapp.com",
+  projectId: "camport-b643b",
+  storageBucket: "camport-b643b.appspot.com",
+  messagingSenderId: "447051103945",
+  appId: "1:447051103945:web:aa6528f4d9cfd14647d718",
+  measurementId: "G-HNRV2GVWSX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
