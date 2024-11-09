a79f6907b58e5333332700efe6f10b43fa228ed9
camden.warburton <camden.warburton@ucalgary.ca>
2024-11-09 14:17:02 -0700

    Fixing priv key

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -12,13 +12,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQEWMZ1yGvJym2CZAPybtikEUGRQIBZA8",
-  authDomain: "camport-b643b.firebaseapp.com",
-  projectId: "camport-b643b",
-  storageBucket: "camport-b643b.appspot.com",
-  messagingSenderId: "447051103945",
-  appId: "1:447051103945:web:aa6528f4d9cfd14647d718",
-  measurementId: "G-HNRV2GVWSX"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURE_ID
 };
 
 
